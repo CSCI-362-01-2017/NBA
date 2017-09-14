cccd8388981aecd1cff6eddbb249aecdbebc2bb0
Andrew7733 <milleran@g.cofc.edu>
2017-09-14 08:28:12 -0400

    Add files via upload
    MyLIst Bash Script for Deliverable

---
+++ MyScript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Team NBA CSCI 362
+ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+HTTP="/"
+OUTPUT="site-index.html" 
+
+i=0
+echo "<UL>" > $OUTPUT
+for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d| sort`; do
+  path=`basename "$filepath"`
+  echo "  <LI>$path</LI>" >> $OUTPUT
+  echo "  <UL>" >> $OUTPUT
+  for i in `find "$filepath" -maxdepth 1 -mindepth 1 -type f| sort`; do
+    file=`basename "$i"`
+    echo "    <LI><a href=\"/$path/$file\">$file</a></LI>" >> $OUTPUT
+  done
+  echo "  </UL>" >> $OUTPUT
+done
+echo "</UL>" >> $OUTPUT
+xdg-open $OUTPUT
