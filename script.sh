e6bfa49caf659b202ab1abb93b4768cf2b2f0bba
Andrew7733 <milleran@g.cofc.edu>
2017-11-07 08:44:48 -0500

    Delete script.sh

--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-javac -cp ".;'/home/dave/Downloads/sigmah-master/src/main/java/org/sigmah/client/util/ClientUtils.java'" /home/dave/Downloads/sigmah-master/src/main/java/org/sigmah/shared/dto/orgunit/OrgUnitDTO.java 
-java /home/dave/Downloads/sigmah-master/src/main/java/org/sigmah/shared/dto/orgunit/OrgUnitDTO
