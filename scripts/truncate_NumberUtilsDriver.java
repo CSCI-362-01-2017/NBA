9c5781190929266b1da797368b51fb22bf7ba3c5
steelebm <steelebm@g.cofc.edu>
2017-11-20 12:25:16 -0500

    Update truncate_NumberUtilsDriver.java

--- scripts/truncate_NumberUtilsDriver.java
+++ scripts/truncate_NumberUtilsDriver.java
@@ -1,6 +1,18 @@
 import project.NumberUtils;
 
-
+/**
+	 * Driver for the truncate method that truncates the decimal part of a number.
+	 * There are two versions of this method, one which takes one parameter, n, that
+	 * automatically truncates to two decimal points. And another version which takes
+	 * two parameters, n and decimals, which truncates n to (decimals) decimals.
+	 * 
+	 * Truncate:
+	 	* @param n
+	 	*          The number, must not be <code>null</code>.
+	 	* @param decimals
+	 	*          The number of decimals. <code>0</code> will truncate all the decimal part.
+		* @return The truncated number.
+	 */
 public class truncate_NumberUtilsDriver{
 	public static void main (String[] args){
 		String[] inputs = args[0].split("[,]");
