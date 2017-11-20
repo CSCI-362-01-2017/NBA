61cb6c69cb88022372d6e31a66771a34ce978822
steelebm <steelebm@g.cofc.edu>
2017-11-20 12:21:03 -0500

    Update ratio_NumberUtilsDriver.java

--- scripts/ratio_NumberUtilsDriver.java
+++ scripts/ratio_NumberUtilsDriver.java
@@ -1,6 +1,15 @@
 import project.NumberUtils;
 
-
+/**
+	 * Driver for the ratio class that computes a ratio.
+	 * 
+	 * Ratio class:
+	 	* @param n
+	 	*          The number.
+	 	* @param in
+	 	*          The ratio number.
+	 	* @return The ratio.
+	 */
 public class ratio_NumberUtilsDriver{
 	public static void main (String[] args){
 		String[] inputs = args[0].split("[,]");
