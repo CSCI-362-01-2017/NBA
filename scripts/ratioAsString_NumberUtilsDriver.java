48c7c9efa5c718c8baa3bd7313fd89a878a17930
steelebm <steelebm@g.cofc.edu>
2017-11-20 12:20:15 -0500

    Update ratioAsString_NumberUtilsDriver.java

--- scripts/ratioAsString_NumberUtilsDriver.java
+++ scripts/ratioAsString_NumberUtilsDriver.java
@@ -1,6 +1,16 @@
 import project.NumberUtils;
 
-
+/**
+	 * Driver for the ratioAsString method that
+	 * computes a ratio and returns it as string.
+	 * 
+	 *ratioAsString:
+	 	* @param n
+		*          The number.
+		* @param in
+	 	*          The ratio number.
+	 	* @return The ratio.
+*/
 public class ratioAsString_NumberUtilsDriver{
 	public static void main (String[] args){
 		String[] inputs = args[0].split("[,]");
